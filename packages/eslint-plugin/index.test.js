47a7472a78749b22b68bf915d69395f5fb5f5257
Karolis Šarapnickis <hi@karolis.sh>
2023-01-18 09:39:23 +0200

    test: fix flaky tests

--- packages/eslint-plugin/index.test.js
+++ packages/eslint-plugin/index.test.js
@@ -1,11 +1,23 @@
+const path = require('path');
+
 const { ESLint } = require('eslint');
 
 const { configs } = require('.');
+
+const NODE_MODULES_DIR = path.resolve(
+  path.dirname(require.resolve('eslint/package.json')),
+  '..',
+);
 
 Object.entries(configs).forEach(([name, config]) => {
   it(`should load ${name} configuration`, async () => {
     const eslint = new ESLint({ useEslintrc: false, baseConfig: config });
     const result = await eslint.calculateConfigForFile('index.js');
+
+    if (result.parser?.startsWith(NODE_MODULES_DIR)) {
+      result.parser = result.parser.replace(NODE_MODULES_DIR, '<node_modules>');
+    }
+
     expect(result).toMatchSnapshot();
   });
 });
