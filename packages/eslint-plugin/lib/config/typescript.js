33d10c54b6ecb24b01630e214bb78d7d39a6221b
Karolis Šarapnickis <hi@karolis.sh>
2023-01-18 08:14:58 +0200

    fix(eslint-plugin): allow typescript plugin to be used as root
    BREAKING CHANGE: typescript plugin no longer work as an override over .ts and .tsx files

--- packages/eslint-plugin/lib/config/typescript.js
+++ packages/eslint-plugin/lib/config/typescript.js
@@ -1,26 +1,20 @@
 module.exports = {
-  extends: ['plugin:@k.sh/esnext'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: [
-        'plugin:@k.sh/esnext',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-      ],
-      plugins: ['@typescript-eslint'],
-      parser: '@typescript-eslint/parser',
-      settings: {
-        'import/resolver': {
-          typescript: {},
-        },
-      },
-      rules: {
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          { ignoreRestSiblings: true },
-        ],
-      },
-    },
+  extends: [
+    'plugin:@k.sh/esnext',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
   ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
+  },
 };
